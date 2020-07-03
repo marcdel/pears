acd1f515bd7170d7cb25d4f529db60bc996a3e73
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-07-03 16:43:01 -0700

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :pears,
+  ecto_repos: [Pears.Repo]
+
+# Configures the endpoint
+config :pears, PearsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "yaoPrOOBTKpirt8RtNjlCx1iK0beb0ojiT7hPcO7Idnj1+mW0EEQ/YW604UN+dYm",
+  render_errors: [view: PearsWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Pears.PubSub,
+  live_view: [signing_salt: "hrGJfYm/"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
