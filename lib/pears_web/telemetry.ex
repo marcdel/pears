f17290a588831a194ae0fb77bdfb57bd57e5b7dc
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-08-12 20:11:47 -0700

    split out different types of metrics and only console log app metrics

--- lib/pears_web/telemetry.ex
+++ lib/pears_web/telemetry.ex
@@ -13,40 +13,52 @@ defmodule PearsWeb.Telemetry do
     children = [
       # Telemetry poller will execute the given period measurements
       # every 60_000ms, or 1 minute. Learn more here: https://hexdocs.pm/telemetry_metrics
-      {:telemetry_poller, measurements: periodic_measurements(), period: 60_000}
+      {:telemetry_poller, measurements: periodic_measurements(), period: 60_000},
       # Add reporters as children of your supervision tree.
-      # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
+      {Telemetry.Metrics.ConsoleReporter, metrics: app_metrics()}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
   end
 
   def metrics do
+    app_metrics() ++ phoenix_metrics() ++ database_metrics() ++ vm_metrics()
+  end
+
+  defp app_metrics do
     [
-      # Phoenix Metrics
+      summary("pears.teams.count")
+    ]
+  end
+
+  defp phoenix_metrics do
+    [
       summary("phoenix.endpoint.stop.duration",
         unit: {:native, :millisecond}
       ),
       summary("phoenix.router_dispatch.stop.duration",
         tags: [:route],
         unit: {:native, :millisecond}
-      ),
+      )
+    ]
+  end
 
-      # Database Metrics
+  defp database_metrics do
+    [
       summary("pears.repo.query.total_time", unit: {:native, :millisecond}),
       summary("pears.repo.query.decode_time", unit: {:native, :millisecond}),
       summary("pears.repo.query.query_time", unit: {:native, :millisecond}),
       summary("pears.repo.query.queue_time", unit: {:native, :millisecond}),
-      summary("pears.repo.query.idle_time", unit: {:native, :millisecond}),
+      summary("pears.repo.query.idle_time", unit: {:native, :millisecond})
+    ]
+  end
 
-      # VM Metrics
+  defp vm_metrics do
+    [
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
       summary("vm.total_run_queue_lengths.total"),
       summary("vm.total_run_queue_lengths.cpu"),
-      summary("vm.total_run_queue_lengths.io"),
-
-      # App Metrics
-      summary("pears.teams.count")
+      summary("vm.total_run_queue_lengths.io")
     ]
   end
 
