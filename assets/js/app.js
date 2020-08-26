b5b0c716f905aa366b1c39729f5ebf8bcffd03d0
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-08-26 12:35:42 -0700

    focus text inputs when they are shown (so user doesn't have to click)

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,8 +17,19 @@ import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
+let Hooks = {}
+Hooks.FocusInput = {
+  mounted(){
+    this.el.focus();
+
+    // Put cursor at the end of the text content
+    this.el.setSelectionRange(-1, -1)
+  }
+};
+
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
