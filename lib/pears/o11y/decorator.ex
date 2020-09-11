a12d64f323aeebb75abedd8bc9a19c1f0f72cc0d
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-09-10 21:53:41 -0700

    handle nested keys and adds tests around some of these helper methods

--- lib/pears/o11y/decorator.ex
+++ lib/pears/o11y/decorator.ex
@@ -3,11 +3,14 @@ defmodule Pears.O11y.Decorator do
 
   def trace(event_name, attr_keys \\ [], body, context) do
     validate_args(event_name, attr_keys)
+
     event_name = Enum.join(event_name, ".")
 
     quote location: :keep do
       require OpenTelemetry.Span
       require OpenTelemetry.Tracer
+
+      alias Pears.O11y.Decorator
 
       maybe_add_team_name = fn attrs ->
         if Keyword.has_key?(attrs, :team) do
@@ -17,34 +20,6 @@ defmodule Pears.O11y.Decorator do
         end
       end
 
-      maybe_add_result = fn attrs, attr_keys, result ->
-        if Enum.member?(attr_keys, :result) do
-          Keyword.put_new(attrs, :result, result)
-        else
-          attrs
-        end
-      end
-
-      remove_underscores = fn attrs ->
-        Enum.map(attrs, fn {k, v} ->
-          k =
-            k
-            |> Atom.to_string()
-            |> String.trim_leading("_")
-            |> String.to_existing_atom()
-
-          {k, v}
-        end)
-      end
-
-      pretty_print = fn attrs ->
-        Enum.map(attrs, fn {key, value} -> {key, Pears.O11y.pretty_inspect(value)} end)
-      end
-
-      not_so_pretty_print = fn attrs ->
-        Enum.map(attrs, fn {key, value} -> {key, Pears.O11y.inspect(value)} end)
-      end
-
       parent_ctx = OpenTelemetry.Tracer.current_span_ctx()
 
       OpenTelemetry.Tracer.with_span unquote(event_name), %{parent: parent_ctx} do
@@ -52,12 +27,12 @@ defmodule Pears.O11y.Decorator do
 
         reportable_attrs =
           Kernel.binding()
-          |> Keyword.take(unquote(attr_keys))
+          |> Decorator.take_attrs(unquote(attr_keys))
           |> maybe_add_team_name.()
           # |> Keyword.put_new(:result, result)
-          |> maybe_add_result.(unquote(attr_keys), result)
-          |> remove_underscores.()
-          |> not_so_pretty_print.()
+          |> Decorator.maybe_add_result(unquote(attr_keys), result)
+          |> Decorator.remove_underscores()
+          |> Decorator.stringify_list()
           |> Enum.into([])
 
         OpenTelemetry.Span.set_attributes(reportable_attrs)
@@ -71,14 +46,71 @@ defmodule Pears.O11y.Decorator do
       reraise %ArgumentError{message: "#{target} #{e.message}"}, __STACKTRACE__
   end
 
-  defp validate_args(event_name, attr_keys) do
+  def take_attrs(bound_values, attr_keys) do
+    {keys, nested_keys} = Enum.split_with(attr_keys, &is_atom/1)
+
+    attrs = Keyword.take(bound_values, keys)
+
+    nested_attrs =
+      Enum.map(nested_keys, fn key_list ->
+        key = key_list |> Enum.join("_") |> String.to_atom()
+        {obj_key, other_keys} = List.pop_at(key_list, 0)
+        value = get_in(bound_values, [obj_key | Enum.map(other_keys, &Access.key(&1, nil))])
+        {key, value}
+      end)
+
+    Keyword.merge(nested_attrs, attrs)
+  end
+
+  def maybe_add_result(attrs, attr_keys, result) do
+    if Enum.member?(attr_keys, :result) do
+      Keyword.put_new(attrs, :result, result)
+    else
+      attrs
+    end
+  end
+
+  def remove_underscores(attrs) do
+    Enum.map(attrs, fn {key, value} ->
+      key =
+        key
+        |> Atom.to_string()
+        |> String.trim_leading("_")
+        |> String.to_existing_atom()
+
+      {key, value}
+    end)
+  end
+
+  def stringify_list(attrs) do
+    Enum.map(attrs, fn {key, value} -> {key, Pears.O11y.inspect(value)} end)
+  end
+
+  def stringify(thing) when is_map(thing) or is_struct(thing) or is_list(thing) do
+    inspect(thing)
+  end
+
+  def stringify(thing), do: thing
+
+  def validate_args(event_name, attr_keys) do
     if not (is_list(event_name) and atoms_only?(event_name) and not Enum.empty?(event_name)),
       do: raise(ArgumentError, "event_name must be a non-empty list of atoms")
 
     if Enum.empty?(event_name), do: raise(ArgumentError, "event_name is empty")
 
-    if not (is_list(attr_keys) and atoms_only?(attr_keys)),
-      do: raise(ArgumentError, "include option must be a list of atoms")
+    if not (is_list(attr_keys) and atoms_or_lists_of_atoms_only?(attr_keys)),
+      do:
+        raise(ArgumentError, "attr_keys must be a list of atoms, including nested lists of atoms")
+  end
+
+  defp atoms_or_lists_of_atoms_only?(list) when is_list(list) do
+    Enum.all?(list, fn item ->
+      (is_list(item) && atoms_or_lists_of_atoms_only?(item)) or is_atom(item)
+    end)
+  end
+
+  defp atoms_or_lists_of_atoms_only?(item) when is_atom(item) do
+    true
   end
 
   defp atoms_only?(list), do: Enum.all?(list, &is_atom/1)
---
+++ test/pears/o11y/decorator_test.exs
@@ -0,0 +1,137 @@
+defmodule Pears.O11y.DecoratorTest do
+  use ExUnit.Case, async: true
+
+  alias Pears.O11y.Decorator
+
+  describe "validate_args" do
+    test "event name must be a non-empty list of atoms" do
+      Decorator.validate_args([:name, :space, :event], [])
+
+      assert_raise ArgumentError, ~r/^event_name/, fn ->
+        Decorator.validate_args("name.space.event", [])
+      end
+    end
+
+    test "attr_keys can be empty" do
+      Decorator.validate_args([:name, :space, :event], [])
+    end
+
+    test "attrs_keys must be atoms" do
+      Decorator.validate_args([:name, :space, :event], [:variable])
+
+      assert_raise ArgumentError, ~r/^attr_keys/, fn ->
+        Decorator.validate_args([:name, :space, :event], ["variable"])
+      end
+    end
+
+    test "attrs_keys can contain nested lists of atoms" do
+      Decorator.validate_args([:name, :space, :event], [:variable, [:obj, :key]])
+    end
+  end
+
+  describe "take_attrs" do
+    test "handles flat attributes" do
+      assert Decorator.take_attrs([id: 1], [:id]) == [id: 1]
+    end
+
+    test "handles nested attributes" do
+      assert Decorator.take_attrs([team: %{id: 1}], [[:team, :id]]) == [team_id: 1]
+    end
+
+    test "handles flat and nested attributes" do
+      attrs = Decorator.take_attrs([error: "whoops", team: %{id: 1}], [:error, [:team, :id]])
+      assert attrs == [team_id: 1, error: "whoops"]
+    end
+
+    test "can take the top level element and a nested attribute" do
+      attrs = Decorator.take_attrs([team: %{id: 1}], [:team, [:team, :id]])
+      assert attrs == [team_id: 1, team: %{id: 1}]
+    end
+
+    test "handles multiply nested attributes" do
+      attrs = Decorator.take_attrs([team: %{pear: %{id: 2}}], [[:team, :pear, :id]])
+      assert attrs == [team_pear_id: 2]
+
+      attrs =
+        Decorator.take_attrs(
+          [team: %{pear: %{track: %{id: 3}}}],
+          [[:team, :pear, :track, :id]]
+        )
+
+      assert attrs == [team_pear_track_id: 3]
+    end
+  end
+
+  describe "maybe_add_result" do
+    test "when :result is given, adds result to the list" do
+      attrs = Decorator.maybe_add_result([], [:result], {:ok, "include me"})
+      assert attrs == [result: {:ok, "include me"}]
+
+      attrs = Decorator.maybe_add_result([id: 10], [:result, :id], {:ok, "include me"})
+      assert attrs == [result: {:ok, "include me"}, id: 10]
+    end
+
+    test "when :result is missing, does not add result to the list" do
+      attrs = Decorator.maybe_add_result([], [], {:ok, "include me"})
+      assert attrs == []
+
+      attrs = Decorator.maybe_add_result([name: "blah"], [:name], {:ok, "include me"})
+      assert attrs == [name: "blah"]
+    end
+  end
+
+  describe "remove_underscores" do
+    test "removes underscores from keys" do
+      assert Decorator.remove_underscores(_id: 1) == [id: 1]
+      assert Decorator.remove_underscores(_id: 1, _name: "asd") == [id: 1, name: "asd"]
+    end
+
+    test "doesn't modify keys without underscores" do
+      assert Decorator.remove_underscores(_id: 1, name: "asd") == [id: 1, name: "asd"]
+    end
+  end
+
+  describe "stringify_list" do
+    test "doesn't modify strings" do
+      attrs = Decorator.stringify_list(string_attr: "hello")
+      assert attrs == [string_attr: "hello"]
+    end
+
+    test "doesn't modify integers" do
+      attrs = Decorator.stringify_list(int_attr: 12)
+      assert attrs == [int_attr: 12]
+    end
+
+    test "stringifies maps" do
+      attrs = Decorator.stringify_list(team: %{id: 10})
+      assert attrs == [team: "%{id: 10}"]
+    end
+
+    test "stringifies structs" do
+      attrs =
+        Decorator.stringify_list(
+          team: %Pears.Core.Team{
+            assigned_pears: %{},
+            available_pears: %{},
+            history: [],
+            id: nil,
+            name: nil,
+            tracks: %{}
+          }
+        )
+
+      assert attrs == [
+               team:
+                 "%Pears.Core.Team{assigned_pears: %{}, available_pears: %{}, history: [], id: nil, name: nil, tracks: %{}}"
+             ]
+    end
+
+    test "stringifies lists" do
+      attrs = Decorator.stringify_list(matches: [1, 2, 3, 4])
+      assert attrs == [matches: "[1, 2, 3, 4]"]
+
+      attrs = Decorator.stringify_list(matches: [{"pear 1", "pear 2"}, {"pear 3", "pear 4"}])
+      assert attrs == [matches: "[{\"pear 1\", \"pear 2\"}, {\"pear 3\", \"pear 4\"}]"]
+    end
+  end
+end
