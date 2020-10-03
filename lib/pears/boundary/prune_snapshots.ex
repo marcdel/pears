65e33970bd86eafc6a84bcf7e0a51cfb82e2889d
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-10-02 21:50:07 -0700

    this module doesn't really seem necessary

--- lib/pears/boundary/prune_snapshots.ex
+++ lib/pears/boundary/prune_snapshots.ex
@@ -4,14 +4,36 @@ defmodule Pears.Boundary.PruneSnapshots do
   so we prune all but the most recent snapshots for each team.
   """
 
-  use PeriodicTask, interval: :timer.hours(1)
+  use GenServer
   use OpenTelemetryDecorator
 
   alias Pears.Persistence.RecordCounts
   alias Pears.Persistence.Snapshots
 
+  @interval :timer.hours(1)
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl GenServer
+  def init(_opts) do
+    send(self(), :prune_snapshots)
+    {:ok, RecordCounts.snapshot_count()}
+  end
+
+  @impl GenServer
+  def handle_info(:prune_snapshots, snapshot_count) do
+    schedule_next()
+    prune_snapshots(snapshot_count)
+  end
+
+  def schedule_next do
+    Process.send_after(self(), :prune_snapshots, @interval)
+  end
+
   @decorate trace("prune_snapshots", include: [:_snapshot_count, :updated_snapshot_count])
-  def handle_tick(_snapshot_count) do
+  def prune_snapshots(_snapshot_count) do
     Snapshots.prune_all()
     updated_snapshot_count = RecordCounts.snapshot_count()
 
