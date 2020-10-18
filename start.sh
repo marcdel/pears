bd5e10f9619e48b6448f7ac439dde3b22a93c09c
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-10-17 21:07:06 -0700

    update deps

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 
 # docker run -d -p 9411:9411 openzipkin/zipkin
 # source .env && docker run -p 9411:9411 honeycombio/honeycomb-opentracing-proxy -k $HONEYCOMB_KEY -d traces
-source .env && iex -S mix phx.server
+source .env &&
+mix deps.get &&
+(cd assets && npm install) &&
+iex -S mix phx.server
