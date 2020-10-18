7f1ee9328a70e7a904d84402dec82f4cd782c842
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-10-18 00:09:16 -0700

    update to open_telemetry_decorator 0.5.1

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Pears.MixProject do
       {:poison, ">= 1.5.0"},
       {:hackney, ">= 1.11.0"},
       {:opentelemetry, "~> 0.4.0"},
-      {:open_telemetry_decorator, "~> 0.5.0"},
+      {:open_telemetry_decorator, "~> 0.5.1"},
       {:opentelemetry_ecto, "~> 0.1.0"},
       {:opentelemetry_honeycomb, "~> 0.3.0-rc.0"},
       {:fun_with_flags, "~> 1.5.1"},
