aa36c2f73bc7682698872aef67d4981eccfe3b8e
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-05-03 22:43:55 -0700

    move heex out of the live.ex file

--- lib/pears_web/live/pairing_board_live.ex
+++ lib/pears_web/live/pairing_board_live.ex
@@ -1,12 +1,6 @@
 defmodule PearsWeb.PairingBoardLive do
   use PearsWeb, :live_view
 
-  def render(assigns) do
-    ~H"""
-    <.header><%= @current_name %></.header>
-    """
-  end
-
   def mount(_params, _session, socket) do
     team = socket.assigns.current_team
     {:ok, assign(socket, :current_name, team.name)}
