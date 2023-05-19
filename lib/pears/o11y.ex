25ec5201e1c5731041c95521322f8bf06a237676
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-05-19 12:53:15 -0700

    need to replace this once all the code is copied over

--- lib/pears/o11y.ex
+++ lib/pears/o11y.ex
@@ -1,16 +1,14 @@
+# TODO: Find and replace references to this
 defmodule Pears.O11y do
-  require OpenTelemetry.Span, as: Span
-  require OpenTelemetry.Tracer, as: Tracer
+  alias OpenTelemetryDecorator.Attributes
 
   @spec set_attribute(OpenTelemetry.attribute_key(), OpenTelemetry.attribute_value()) :: boolean()
   def set_attribute(key, value) do
-    ctx = Tracer.current_span_ctx()
-    Span.set_attribute(ctx, key, value)
+    Attributes.set(key, value)
   end
 
   @spec set_attributes([OpenTelemetry.attribute()]) :: boolean()
   def set_attributes(attrs) do
-    ctx = Tracer.current_span_ctx()
-    Span.set_attributes(ctx, attrs)
+    Attributes.set(attrs)
   end
 end
