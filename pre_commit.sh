9292dec1815cd11d703301069c130addd81f4e1a
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-05-19 13:06:19 -0700

    update precommit script

--- pre_commit.sh
+++ pre_commit.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
-source .env &&
-mix compile --warnings-as-errors &&
-mix format &&
-mix credo --strict &&
-mix test &&
-#(cd ui_tests && npx cypress run) &&
-cat << EOF
+source .env
+
+mix deps.unlock --unused
+mix deps.clean --unused
+mix compile --warnings-as-errors
+
+mix format
+mix credo --strict
+
+TELEMETRY_LOG_LEVEL=debug mix test
+#(cd ui_tests && npx cypress run)
+
+cat <<EOF
  ___ _   _  ___ ___ ___  ___ ___
 / __| | | |/ __/ __/ _ \/ __/ __|
 \__ \ |_| | (_| (_|  __/\__ \__ \\
