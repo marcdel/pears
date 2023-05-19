1c7a47cee505776fb995e7948c24e6d1876ca887
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-05-19 12:43:47 -0700

    change team.name to team_name

--- lib/pears_web/live/pairing_board_live.ex
+++ lib/pears_web/live/pairing_board_live.ex
@@ -2,9 +2,9 @@ defmodule PearsWeb.PairingBoardLive do
   use PearsWeb, :live_view
   use OpenTelemetryDecorator
 
-  @decorate trace("team_live.mount", include: [[:team, :name]])
+  @decorate trace("team_live.mount", include: [:team_name])
   def mount(_params, _session, socket) do
-    team = socket.assigns.current_team
-    {:ok, assign(socket, :current_name, team.name)}
+    %{name: team_name} = socket.assigns.current_team
+    {:ok, assign(socket, :current_name, team_name)}
   end
 end
