b6f6a613e605632d2545b10f75acc8e9b7522b19
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-05-24 23:02:32 -0700

    fix drag and drop hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,78 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Drag from './dragHook'
+
+let Hooks = {}
+Hooks.Drag = Drag
+
+Hooks.FocusInput = {
+  mounted() {
+    this.el.focus()
+
+    // Put cursor at the end of the text content
+    this.el.setSelectionRange(-1, -1)
+
+    this.el.addEventListener("keyup", e => {
+      if (e.key == "Escape") {
+        e.preventDefault()
+        this.pushEvent("cancel-editing-track")
+      }
+    })
+  }
+}
+
+Hooks.Pear = {
+  mounted() {
+    this.el.addEventListener("dragstart", e => {
+      e.dataTransfer.effectAllowed = "move"
+
+      const pearName = e.target.getAttribute("phx-value-pear-name")
+      const currentLocation = e.target.getAttribute("phx-value-current-location")
+
+      this.pushEvent("drag-pear", {"pear-name": pearName, "current-location": currentLocation})
+
+      e.dataTransfer.setData("pear-name", pearName)
+      e.dataTransfer.setData("current-location", currentLocation)
+    })
+
+    this.el.addEventListener("dragover", e => {
+      e.preventDefault()
+    })
+  }
+}
+
+Hooks.Destination = {
+  mounted() {
+    this.el.addEventListener("dragenter", e => {
+      e.target.classList.add("dragged-over")
+    })
+
+    this.el.addEventListener("dragleave", e => {
+      e.target.classList.remove("dragged-over")
+    })
+
+    this.el.addEventListener("dragover", e => {
+      e.preventDefault()
+    })
+
+    this.el.addEventListener("drop", e => {
+      e.preventDefault()
+      e.target.classList.remove("dragged-over")
+
+      let from = event.dataTransfer.getData("current-location")
+      let to = e.target.getAttribute("phx-value-destination")
+      let pear = event.dataTransfer.getData("pear-name")
+
+      console.debug({from, to, pear})
+
+      this.pushEvent("move-pear", {from, to, pear})
+    })
+  }
+}
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
