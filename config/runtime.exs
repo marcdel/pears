a39568c4a3fd73fa54719fe145688670782c501f
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-07-07 15:07:37 -0700

    try extracting shared setup steps

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,9 +7,9 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 if config_env() == :prod do
-  config :pears, slack_client_id: Map.fetch!(System.get_env(), "SLACK_CLIENT_ID")
-  config :pears, slack_client_secret: Map.fetch!(System.get_env(), "SLACK_CLIENT_SECRET")
-  config :pears, slack_oauth_redirect_uri: Map.get(System.get_env(), "SLACK_OAUTH_URL")
+  config :pears, slack_client_id: System.fetch_env!("SLACK_CLIENT_ID")
+  config :pears, slack_client_secret: System.fetch_env!("SLACK_CLIENT_SECRET")
+  config :pears, slack_oauth_redirect_uri: System.get_env("SLACK_OAUTH_URL")
 
   database_url =
     System.get_env("DATABASE_URL") ||
