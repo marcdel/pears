c02fe0d9d320ab142fb858160503aefd0eb4fc4b
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-10-05 22:20:40 -0700

    update dependencies

--- lib/pears_web/components/facilitator_message.ex
+++ lib/pears_web/components/facilitator_message.ex
@@ -3,14 +3,9 @@ defmodule PearsWeb.FacilitatorMessage do
   use OpenTelemetryDecorator
 
   @impl true
-  def preload([assigns]) do
-    {:ok, facilitator} = Pears.facilitator(assigns.team_name)
-    [Map.put(assigns, :facilitator, facilitator.name)]
-  end
-
-  @impl true
   def mount(socket) do
-    {:ok, socket}
+    {:ok, facilitator} = Pears.facilitator(socket.assigns.team_name)
+    {:ok, assign(socket, :facilitator, facilitator.name)}
   end
 
   @impl true
